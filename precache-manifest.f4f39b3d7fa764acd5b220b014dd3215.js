10e81fc708c1732811a1b5b65d367056a217b3b9
Afa <ifundeasy@gmail.com>
2020-05-02 22:27:19 +0700

    Updates

---
+++ precache-manifest.f4f39b3d7fa764acd5b220b014dd3215.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d65cc5efdfd80210a44c8cdf7e25ae64",
+    "url": "./index.html"
+  },
+  {
+    "revision": "e0fe1bb6fcd2cad4710c",
+    "url": "./static/css/main.16724e62.chunk.css"
+  },
+  {
+    "revision": "a3ebc7f0d838d16e7348",
+    "url": "./static/js/2.e9a1fc97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e9a1fc97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0fe1bb6fcd2cad4710c",
+    "url": "./static/js/main.39bf3b38.chunk.js"
+  },
+  {
+    "revision": "d197a2870f77bdc70c40",
+    "url": "./static/js/runtime-main.42880fac.js"
+  }
+]);
